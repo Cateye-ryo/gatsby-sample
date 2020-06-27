8f9ee75855857eac3af62af8de23a2f48f9ab970
Cateye-ryo <cateye.ryo@gmail.com>
2020-06-27 15:23:27 +0900

    end 3-3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
+import Header from "../components/header"
+import Footer from "../components/footer"
+
 export default ({ data }) => (
 <div>
   <meta charSet="UTF-8" />
@@ -10,24 +13,14 @@ export default ({ data }) => (
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" integrity="sha384-v8BU367qNbs/aIZIxuivaU55N5GPF89WBerHoGA4QTcbUjYiLQtKdrfXnqAcXyTv" crossOrigin="anonymous" />
   <link rel="stylesheet" href="style.css" />
   <link rel="icon" href="images/icon.png" type="image/png" />
-  <header className="header">
-    <div className="container">
-      <div className="site">
-        <a href="base-index.html">
-          <img src="images/logo.svg" alt="ESSENTIALS" />
-        </a>
-      </div>
-      <nav className="nav">
-        <ul>
-          <li><a href="base-index.html">TOP</a></li>
-          <li><a href="base-about.html">ABOUT</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+  <Header />
   <section className="hero">
     <figure>
-      <Img fluid={ data.hero.childImageSharp.fluid } alt=""/>
+      <Img 
+        fluid={ data.hero.childImageSharp.fluid } 
+        alt=""
+        style={{ height: "100%"}}
+      />
     </figure>
     <div className="catch">
       <h1>There is no love sincerer than<br /> the love of food.</h1>
@@ -71,39 +64,14 @@ export default ({ data }) => (
   <section className="photo">
     <h2 className="sr-only">Photo</h2>
     <figure>
-      <Img fluid={ data.berry.childImageSharp.fluid } alt=""/>
+      <Img 
+        fluid={ data.berry.childImageSharp.fluid } 
+        alt=""
+        style={{ height: "100%"}}
+      />
     </figure>
   </section>
-  <footer className="footer">
-    <div className="container">
-      <div className="site">
-        <a href="base-index.html">
-          <img src="images/logo-w.svg" alt="ESSENTIALS" />
-          <p>おいしい食材と食事を探求するサイト</p>
-        </a>
-      </div>
-      <ul className="sns">
-        <li>
-          <a href="https://twitter.com/">
-            <i className="fab fa-twitter" />
-            <span className="sr-only">Twitter</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://facebook.com/">
-            <i className="fab fa-facebook-square" />
-            <span className="sr-only">Facebook</span>
-          </a>
-        </li>
-        <li>
-          <a href="http://instagram.com/">
-            <i className="fab fa-instagram" />
-            <span className="sr-only">Instagram</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </footer>
+  <Footer />
 </div>
 )
 export const query = graphql`
