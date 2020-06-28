16e35ad14a353a3cf084ae8ee96659b01181a988
Cateye-ryo <cateye.ryo@gmail.com>
2020-06-28 22:11:11 +0900

    end step 4

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,10 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
+import { Link } from "gatsby"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTwitter, faFacebookSquare, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 export default() => {
   const data = useStaticQuery(graphql`
@@ -18,27 +22,27 @@ export default() => {
   <footer className="footer">
     <div className="container">
       <div className="site">
-        <a href="base-index.html">
+        <Link to={`/`}>
           <img src="images/logo-w.svg" alt="ESSENTIALS" />
           <p>おいしい食材と食事を探求するサイト</p>
-        </a>
+        </Link>
       </div>
       <ul className="sns">
         <li>
           <a href="https://twitter.com/">
-            <i className="fab fa-twitter" />
+            < FontAwesomeIcon icon={faTwitter} />
             <span className="sr-only">Twitter</span>
           </a>
         </li>
         <li>
           <a href="https://facebook.com/">
-            <i className="fab fa-facebook-square" />
+            < FontAwesomeIcon icon={faFacebookSquare} />
             <span className="sr-only">Facebook</span>
           </a>
         </li>
         <li>
           <a href="http://instagram.com/">
-            <i className="fab fa-instagram" />
+            < FontAwesomeIcon icon={faInstagram} />
             <span className="sr-only">Instagram</span>
           </a>
         </li>
